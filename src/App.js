319edb6ea67256b7c0991f92c7902f6acb304cf5
Williams Balogun <williamsdev.official@gmail.com>
2023-01-11 00:15:54 +0100

    feat: positioned item to scale and integrated the Dexla signature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import Vector from './asset/Vector.png'
+import Logos from './asset/Logos.png'
 
 const url = 'https://api.figma.com/v1/files/vRCqsniN1t2PndqlKeYQwI'
 
@@ -125,6 +126,17 @@ const App = () => {
       return { ...item }
     })
 
+  // DEXLA SIGNATURE
+  const dexla = obj
+    .filter((item) => item.id === '201:157')
+    .reduce((item) => {
+      return { ...item }
+    })
+
+  const dexlaStrokes = dexla.strokes.reduce((item) => {
+    return { ...item }
+  })
+
   // THE BODY
   return (
     <main
@@ -150,11 +162,7 @@ const App = () => {
               color.g * c
             }, ${color.b * c},${color.a})`,
             background: `rgba(${bcg.r * c},${bcg.g * c},${bcg.b * c},${bcg.a})`,
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            gap: '24px',
+            position: 'relative',
           }}>
           <div
             style={{
@@ -164,6 +172,10 @@ const App = () => {
                 icon.backgroundColor.g * c
               }, ${icon.backgroundColor.b * c}, ${icon.backgroundColor.a})`,
               borderRadius: `${icon.cornerRadius}px`,
+              position: 'absolute',
+              left: '50%',
+              top: `${icon.absoluteBoundingBox.y}px`,
+              transform: `translateX(${icon.absoluteBoundingBox.x}px)`,
               display: 'flex',
               justifyContent: 'center',
               alignItems: 'center',
@@ -179,13 +191,12 @@ const App = () => {
           </div>
           <div
             style={{
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center',
-              gap: '8px',
               width: `${frame.absoluteBoundingBox.width}px`,
               height: `${frame.absoluteBoundingBox.height}px`,
+              position: 'absolute',
+              left: '50%',
+              top: `${frame.absoluteBoundingBox.y}px`,
+              transform: `translateX(${frame.absoluteBoundingBox.x}px)`,
               backgroundColor: `rgba(${frame.backgroundColor.r * c}, ${
                 frame.backgroundColor.g * c
               }, ${frame.backgroundColor.b * c},${frame.backgroundColor.a})`,
@@ -202,6 +213,10 @@ const App = () => {
                     height: `${item.absoluteBoundingBox.height}px`,
                     width: `${item.absoluteBoundingBox.width}px`,
                     textAlign: `${item.style.textAlignHorizontal}`,
+                    position: 'absolute',
+                    top: `${
+                      item.absoluteBoundingBox.y - frame.absoluteBoundingBox.y
+                    }px`,
                   }}
                   className='text'
                   key={item.id}>
@@ -209,6 +224,86 @@ const App = () => {
                 </p>
               )
             })}
+          </div>
+          <div
+            style={{
+              width: `${dexla.absoluteBoundingBox.width}px`,
+              height: `${dexla.absoluteBoundingBox.height}px`,
+              position: 'absolute',
+              top: `${dexla.absoluteBoundingBox.y}px`,
+              left: '50%',
+              transform: `translateX(${dexla.absoluteBoundingBox.x}px)`,
+              background: `rgba(${dexla.backgroundColor.r * c},${
+                dexla.backgroundColor.g * c
+              },${dexla.backgroundColor.b * c},${dexla.backgroundColor.a})`,
+              borderRadius: `${dexla.cornerRadius}px`,
+              border: `${strokeWeight}px ${dexlaStrokes.type} rgba(${
+                dexlaStrokes.color.r * c
+              }, ${dexlaStrokes.color.g * c},${dexlaStrokes.color.b * c},${
+                dexlaStrokes.color.a
+              })`,
+              padding: `${dexla.paddingTop}px ${dexla.paddingRight}px ${dexla.paddingBottom}px ${dexla.paddingLeft}px`,
+              display: 'flex',
+              justifyContent: `${dexla.primaryAxisAlignItems}`,
+              gap: `${dexla.itemSpacing}px`,
+            }}>
+            {dexla.children
+              .filter((dex) => dex.name === 'Logos')
+              .map((item) => {
+                return (
+                  <img
+                    key={item.id}
+                    style={{
+                      width: `${item.absoluteBoundingBox.width}px`,
+                      height: `${item.absoluteBoundingBox.height}px`,
+                      backgroundColor: `rgba(${item.backgroundColor.r * c}px,${
+                        item.backgroundColor.r * c
+                      },${item.backgroundColor.g * c},${
+                        item.backgroundColor.b * c
+                      },${item.backgroundColor.a})`,
+                    }}
+                    src={Logos}
+                    alt={item.name}
+                  />
+                )
+              })}
+            {dexla.children
+              .filter((dex) => dex.id === '201:159')
+              .map((item) => {
+                const { color: cl } = item.fills.reduce((col) => {
+                  return { ...col }
+                })
+                return (
+                  <p
+                    key={item.id}
+                    style={{
+                      width: `${item.absoluteBoundingBox.width}px`,
+                      height: `${item.absoluteBoundingBox.height}px`,
+                      fontFamily: `${item.style.fontFamily}`,
+                      fontWeight: `${item.style.fontWeight}`,
+                      fontSize: `${item.style.fontSize}px`,
+                      letterSpacing: `${item.style.letterSpacing}px`,
+                      lineHeight: `${item.style.lineHeightPx}px`,
+                      textAlign: `${item.style.textAlignHorizontal}`,
+                    }}>
+                    {item.characters.split(' ').map((text, index) => {
+                      let textCol =
+                        text === 'Dexla'
+                          ? `rgba(${cl.r * c},${cl.g * c},${cl.b * c},${cl.a})`
+                          : `rgba(${c},${c},${c}, 1)`
+
+                      return (
+                        <span
+                          style={{
+                            color: `${textCol}`,
+                          }}>
+                          {text}{' '}
+                        </span>
+                      )
+                    })}
+                  </p>
+                )
+              })}
           </div>
         </article>
       )}
