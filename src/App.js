0dc0f54482e49230e0a5fcfa6429bdb9ff74d541
Williams Balogun <williamsdev.official@gmail.com>
2023-01-11 18:38:32 +0100

    fix: modified code to cater for new additions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useCallback } from 'react'
 import Vector from './asset/Vector.png'
 import Logos from './asset/Logos.png'
 
@@ -19,15 +19,6 @@ const requestOptions = {
 const App = () => {
   // STATE CONDITIONS
   const [state, setState] = useState({ loading: true, error: false, data: {} })
-  const [view, setView] = useState(false)
-
-  // SET VIEW TO FALSE AFTER 8s IDLE TIME
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      if (view) setView(false)
-    }, 8000)
-    return () => clearTimeout(timeout)
-  }, [view])
 
   // Default rgb value
   const c = 255
@@ -108,35 +99,6 @@ const App = () => {
     return { ...item }
   })
 
-  // ASSET ICON/PORTFOLIO
-  const icon = obj
-    .filter((item) => item.id === '1:11')
-    .reduce((item) => {
-      return { ...item }
-    })
-
-  const iconImage = icon.children.reduce((item) => {
-    return { ...item }
-  })
-
-  // ASSET FRAME 307
-  const frame = obj
-    .filter((item) => item.id === '1:14')
-    .reduce((item) => {
-      return { ...item }
-    })
-
-  // DEXLA SIGNATURE
-  const dexla = obj
-    .filter((item) => item.id === '201:157')
-    .reduce((item) => {
-      return { ...item }
-    })
-
-  const dexlaStrokes = dexla.strokes.reduce((item) => {
-    return { ...item }
-  })
-
   // THE BODY
   return (
     <main
@@ -145,134 +107,267 @@ const App = () => {
         backgroundColor: `rgba(${bg.r * c},${bg.g * c},${bg.b * c}, ${bg.a})`,
       }}>
       <button
-        onClick={() => setView(true)}
-        className={`btn ${!view && 'showbtn'}`}>
-        View Content
-      </button>
-      {view && (
-        <article
-          onClick={() => setView(false)}
-          className='article'
-          id='asset--button'
-          style={{
-            width: `${size.width}px`,
-            height: `${size.height}px`,
-            borderRadius: `${cornerRadius}px`,
-            border: `${strokeWeight}px ${type} rgba(${color.r * c}, ${
-              color.g * c
-            }, ${color.b * c},${color.a})`,
-            background: `rgba(${bcg.r * c},${bcg.g * c},${bcg.b * c},${bcg.a})`,
-            position: 'relative',
-          }}>
-          <div
-            style={{
-              width: `${icon.absoluteBoundingBox.width}px`,
-              height: `${icon.absoluteBoundingBox.height}px`,
-              background: `rgba(${icon.backgroundColor.r * c}, ${
-                icon.backgroundColor.g * c
-              }, ${icon.backgroundColor.b * c}, ${icon.backgroundColor.a})`,
-              borderRadius: `${icon.cornerRadius}px`,
-              position: 'absolute',
-              left: '50%',
-              top: `${icon.absoluteBoundingBox.y}px`,
-              transform: `translateX(${icon.absoluteBoundingBox.x}px)`,
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <img
-              style={{
-                width: `${iconImage.absoluteBoundingBox.width}px`,
-                height: `${iconImage.absoluteBoundingBox.height}px`,
-              }}
-              src={Vector}
-              alt='vector'
-            />
-          </div>
-          <div
-            style={{
-              width: `${frame.absoluteBoundingBox.width}px`,
-              height: `${frame.absoluteBoundingBox.height}px`,
-              position: 'absolute',
-              left: '50%',
-              top: `${frame.absoluteBoundingBox.y}px`,
-              transform: `translateX(${frame.absoluteBoundingBox.x}px)`,
-              backgroundColor: `rgba(${frame.backgroundColor.r * c}, ${
-                frame.backgroundColor.g * c
-              }, ${frame.backgroundColor.b * c},${frame.backgroundColor.a})`,
-            }}>
-            {frame.children.map((item) => {
-              return (
-                <p
-                  style={{
-                    fontFamily: `${item.style.fontFamily}, 'Trebuchet MS'`,
-                    fontSize: `${item.style.fontSize}px`,
-                    fontWeight: `${item.style.fontWeight}`,
-                    letterSpacing: `${item.style.letterSpacing}px`,
-                    lineHeight: `${item.style.lineHeightPx}px`,
-                    height: `${item.absoluteBoundingBox.height}px`,
-                    width: `${item.absoluteBoundingBox.width}px`,
-                    textAlign: `${item.style.textAlignHorizontal}`,
-                    position: 'absolute',
-                    top: `${
-                      item.absoluteBoundingBox.y - frame.absoluteBoundingBox.y
-                    }px`,
-                  }}
-                  className='text'
-                  key={item.id}>
-                  {item.name}
-                </p>
-              )
-            })}
-          </div>
-          <div
-            style={{
-              width: `${dexla.absoluteBoundingBox.width}px`,
-              height: `${dexla.absoluteBoundingBox.height}px`,
-              position: 'absolute',
-              top: `${dexla.absoluteBoundingBox.y}px`,
-              left: '50%',
-              transform: `translateX(${dexla.absoluteBoundingBox.x}px)`,
-              background: `rgba(${dexla.backgroundColor.r * c},${
-                dexla.backgroundColor.g * c
-              },${dexla.backgroundColor.b * c},${dexla.backgroundColor.a})`,
-              borderRadius: `${dexla.cornerRadius}px`,
-              border: `${strokeWeight}px ${dexlaStrokes.type} rgba(${
-                dexlaStrokes.color.r * c
-              }, ${dexlaStrokes.color.g * c},${dexlaStrokes.color.b * c},${
-                dexlaStrokes.color.a
-              })`,
-              padding: `${dexla.paddingTop}px ${dexla.paddingRight}px ${dexla.paddingBottom}px ${dexla.paddingLeft}px`,
-              display: 'flex',
-              justifyContent: `${dexla.primaryAxisAlignItems}`,
-              gap: `${dexla.itemSpacing}px`,
-            }}>
-            {dexla.children
-              .filter((dex) => dex.name === 'Logos')
-              .map((item) => {
-                return (
-                  <img
-                    key={item.id}
-                    style={{
-                      width: `${item.absoluteBoundingBox.width}px`,
-                      height: `${item.absoluteBoundingBox.height}px`,
-                      backgroundColor: `rgba(${item.backgroundColor.r * c}px,${
-                        item.backgroundColor.r * c
-                      },${item.backgroundColor.g * c},${
-                        item.backgroundColor.b * c
-                      },${item.backgroundColor.a})`,
-                    }}
-                    src={Logos}
-                    alt={item.name}
-                  />
-                )
-              })}
-            {dexla.children
-              .filter((dex) => dex.id === '201:159')
-              .map((item) => {
-                const { color: cl } = item.fills.reduce((col) => {
-                  return { ...col }
+        className='article'
+        id='asset--button'
+        style={{
+          width: `${size.width}px`,
+          height: `${size.height}px`,
+          borderRadius: `${cornerRadius}px`,
+          border: `${strokeWeight}px ${type} rgba(${color.r * c}, ${
+            color.g * c
+          }, ${color.b * c},${color.a})`,
+          background: `rgba(${bcg.r * c},${bcg.g * c},${bcg.b * c},${bcg.a})`,
+          position: 'relative',
+        }}>
+        {obj.map((item) => {
+          let frame = null
+          switch (item.id) {
+            case '1:11':
+              frame = obj
+                .filter((item) => item.id === '1:11')
+                .reduce((item) => {
+                  return { ...item }
                 })
+              break
+
+            case '1:14':
+              frame = obj
+                .filter((item) => item.id === '1:14')
+                .reduce((item) => {
+                  return { ...item }
+                })
+              break
+
+            case '201:157':
+              frame = obj
+                .filter((item) => item.id === '201:157')
+                .reduce((item) => {
+                  return { ...item }
+                })
+              break
+
+            case undefined || null || '':
+              frame = item.reduce((val) => {
+                return { ...item }
+              })
+              break
+
+            default:
+              frame = null
+              break
+          }
+
+          if (item === undefined || item === null || !item) {
+            return <div></div>
+          }
+          if (item.id === '1:11') {
+            const iconImage = frame.children.reduce((item) => {
+              return { ...item }
+            })
+            return (
+              <di
+                key={item.id}
+                style={{
+                  width: `${frame.absoluteBoundingBox.width}px`,
+                  height: `${frame.absoluteBoundingBox.height}px`,
+                  background: `rgba(${frame.backgroundColor.r * c}, ${
+                    frame.backgroundColor.g * c
+                  }, ${frame.backgroundColor.b * c}, ${
+                    frame.backgroundColor.a
+                  })`,
+                  borderRadius: `${frame.cornerRadius}px`,
+                  position: 'absolute',
+                  left: '50%',
+                  top: `${frame.absoluteBoundingBox.y}px`,
+                  transform: `translateX(${frame.absoluteBoundingBox.x}px)`,
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <img
+                  style={{
+                    width: `${iconImage.absoluteBoundingBox.width}px`,
+                    height: `${iconImage.absoluteBoundingBox.height}px`,
+                  }}
+                  src={Vector}
+                  alt='vector'
+                />
+              </di>
+            )
+          }
+          if (item.id === '1:14') {
+            return (
+              <div
+                key={item.id}
+                style={{
+                  width: `${frame.absoluteBoundingBox.width}px`,
+                  height: `${frame.absoluteBoundingBox.height}px`,
+                  position: 'absolute',
+                  left: '50%',
+                  top: `${frame.absoluteBoundingBox.y}px`,
+                  transform: `translateX(${frame.absoluteBoundingBox.x}px)`,
+                  backgroundColor: `rgba(${frame.backgroundColor.r * c}, ${
+                    frame.backgroundColor.g * c
+                  }, ${frame.backgroundColor.b * c},${
+                    frame.backgroundColor.a
+                  })`,
+                }}>
+                {frame.children.map((item) => {
+                  return (
+                    <p
+                      style={{
+                        fontFamily: `${item.style.fontFamily}, 'Trebuchet MS'`,
+                        fontSize: `${item.style.fontSize}px`,
+                        fontWeight: `${item.style.fontWeight}`,
+                        letterSpacing: `${item.style.letterSpacing}px`,
+                        lineHeight: `${item.style.lineHeightPx}px`,
+                        height: `${item.absoluteBoundingBox.height}px`,
+                        width: `${item.absoluteBoundingBox.width}px`,
+                        textAlign: `${item.style.textAlignHorizontal}`,
+                        position: 'absolute',
+                        top: `${
+                          item.absoluteBoundingBox.y -
+                          frame.absoluteBoundingBox.y
+                        }px`,
+                      }}
+                      className='text'
+                      key={item.id}>
+                      {item.name}
+                    </p>
+                  )
+                })}
+              </div>
+            )
+          }
+
+          if (item.id === '201:157') {
+            const dexlaStrokes = frame.strokes.reduce((item) => {
+              return { ...item }
+            })
+            return (
+              <div
+                key={item.id}
+                style={{
+                  width: `${frame.absoluteBoundingBox.width}px`,
+                  height: `${frame.absoluteBoundingBox.height}px`,
+                  position: 'absolute',
+                  top: `${frame.absoluteBoundingBox.y}px`,
+                  left: '50%',
+                  transform: `translateX(${frame.absoluteBoundingBox.x}px)`,
+                  background: `rgba(${frame.backgroundColor.r * c},${
+                    frame.backgroundColor.g * c
+                  },${frame.backgroundColor.b * c},${frame.backgroundColor.a})`,
+                  borderRadius: `${frame.cornerRadius}px`,
+                  border: `${strokeWeight}px ${dexlaStrokes.type} rgba(${
+                    dexlaStrokes.color.r * c
+                  }, ${dexlaStrokes.color.g * c},${dexlaStrokes.color.b * c},${
+                    dexlaStrokes.color.a
+                  })`,
+                  padding: `${frame.paddingTop}px ${frame.paddingRight}px ${frame.paddingBottom}px ${frame.paddingLeft}px`,
+                  display: 'flex',
+                  justifyContent: `${frame.primaryAxisAlignItems}`,
+                  gap: `${frame.itemSpacing}px`,
+                }}>
+                {frame.children
+                  .filter((dex) => dex.name === 'Logos')
+                  .map((item) => {
+                    return (
+                      <img
+                        key={item.id}
+                        style={{
+                          width: `${item.absoluteBoundingBox.width}px`,
+                          height: `${item.absoluteBoundingBox.height}px`,
+                          backgroundColor: `rgba(${
+                            item.backgroundColor.r * c
+                          }px,${item.backgroundColor.r * c},${
+                            item.backgroundColor.g * c
+                          },${item.backgroundColor.b * c},${
+                            item.backgroundColor.a
+                          })`,
+                        }}
+                        src={Logos}
+                        alt={item.name}
+                      />
+                    )
+                  })}
+                {frame.children
+                  .filter((dex) => dex.id === '201:159')
+                  .map((item) => {
+                    const { color: cl } = item.fills.reduce((col) => {
+                      return { ...col }
+                    })
+                    return (
+                      <p
+                        key={item.id}
+                        style={{
+                          width: `${item.absoluteBoundingBox.width}px`,
+                          height: `${item.absoluteBoundingBox.height}px`,
+                          fontFamily: `${item.style.fontFamily}`,
+                          fontWeight: `${item.style.fontWeight}`,
+                          fontSize: `${item.style.fontSize}px`,
+                          letterSpacing: `${item.style.letterSpacing}px`,
+                          lineHeight: `${item.style.lineHeightPx}px`,
+                          textAlign: `${item.style.textAlignHorizontal}`,
+                        }}>
+                        {item.characters.split(' ').map((text, index) => {
+                          let textCol =
+                            text === 'Dexla'
+                              ? `rgba(${cl.r * c},${cl.g * c},${cl.b * c},${
+                                  cl.a
+                                })`
+                              : `rgba(${c},${c},${c}, 1)`
+
+                          return (
+                            <span
+                              key={index}
+                              style={{
+                                color: `${textCol}`,
+                              }}>
+                              {text}{' '}
+                            </span>
+                          )
+                        })}
+                      </p>
+                    )
+                  })}
+              </div>
+            )
+          }
+          return (
+            <div
+              key={item.id}
+              style={{
+                width: `${frame.absoluteBoundingBox.width}px`,
+                height: `${frame.absoluteBoundingBox.height}px`,
+                position: 'absolute',
+                top: `${frame.absoluteBoundingBox.y}px`,
+                left: '50%',
+                transform: `translateX(${frame.absoluteBoundingBox.x}px)`,
+                background: `rgba(${frame.backgroundColor.r * c},${
+                  frame.backgroundColor.g * c
+                },${frame.backgroundColor.b * c},${frame.backgroundColor.a})`,
+              }}>
+              {frame.children.map((item) => {
+                if (item.type === 'FRAME') {
+                  return (
+                    <img
+                      key={item.id}
+                      style={{
+                        width: `${item.absoluteBoundingBox.width}px`,
+                        height: `${item.absoluteBoundingBox.height}px`,
+                        backgroundColor: `rgba(${
+                          item.backgroundColor.r * c
+                        }px,${item.backgroundColor.r * c},${
+                          item.backgroundColor.g * c
+                        },${item.backgroundColor.b * c},${
+                          item.backgroundColor.a
+                        })`,
+                      }}
+                      src={Logos}
+                      alt={item.name}
+                    />
+                  )
+                }
                 return (
                   <p
                     key={item.id}
@@ -286,27 +381,14 @@ const App = () => {
                       lineHeight: `${item.style.lineHeightPx}px`,
                       textAlign: `${item.style.textAlignHorizontal}`,
                     }}>
-                    {item.characters.split(' ').map((text, index) => {
-                      let textCol =
-                        text === 'Dexla'
-                          ? `rgba(${cl.r * c},${cl.g * c},${cl.b * c},${cl.a})`
-                          : `rgba(${c},${c},${c}, 1)`
-
-                      return (
-                        <span
-                          style={{
-                            color: `${textCol}`,
-                          }}>
-                          {text}{' '}
-                        </span>
-                      )
-                    })}
+                    {item.name}
                   </p>
                 )
               })}
-          </div>
-        </article>
-      )}
+            </div>
+          )
+        })}
+      </button>
     </main>
   )
 }
